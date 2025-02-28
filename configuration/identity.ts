bc5a6e5cc580c5c8b8c79aa9bb91e7f8bdd31e8a
sgali1234 <sandeepgali1999@gmail.com>
2025-02-28 17:51:46 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `SANDEEP`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[NBA Connisseur]`;
+export const AI_NAME: string = `[NBA Connoisseur]`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
